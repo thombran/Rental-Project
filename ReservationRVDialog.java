4d32bb780054bc4416e9a43efd0145ac7d05627d
dkuipers13 <65096242+dkuipers13@users.noreply.github.com>
2020-06-11 10:59:53 -0400

    Update ReservationRVDialog.java

--- ReservationRVDialog.java
+++ ReservationRVDialog.java
@@ -114,6 +114,7 @@ public class ReservationRVDialog extends JDialog implements ActionListener {
                 rv.setEstimatedCheckOut(gregTemp);
 
             } catch (ParseException e1) {
+                throw new IllegalArgumentException("Invalid Date Format", e1);
 //                  Do some thing good, what I am not sure.
             }
 
